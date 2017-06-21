390e52b3ea141d8e82377410e4bada5f39d4d626
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-21 21:25:38 +0000

    Added seed users

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({first_name: 'Vasiliy', last_name: "Klimkin", email: "vasily.klimkin@gmail.com"}),
+        knex('users').insert({first_name: 'John', last_name: "Lennie", email: "john.lennie@gmail.com"}),
+        knex('users').insert({first_name: 'David', last_name: "Chasteau", email: "david.chasteau@gmail.com"})
+        //knex('users').insert({id: 2, name: 'Bob'}),
+        //knex('users').insert({id: 3, name: 'Charlie'})
       ]);
     });
 };
