59abb88e4475514963cfb9ca7f517a658712797d
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-22 15:45:31 +0000

    Made alot of data for database

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,12 +1,25 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
+    return Promise.all([
+              // Inserts seed entries
+      knex('users').insert({id: 1, first_name: 'Vasiliy', last_name: "Klimkin", email: "vasily.klimkin@gmail.com"}),
+      knex('users').insert({id: 2, first_name: 'John', last_name: "Lennie", email: "john.lennie@gmail.com"}),
+      knex('users').insert({id: 3, first_name: 'David', last_name: "Chasteau", email: "david.chasteau@gmail.com"}),
+      knex('users').insert({id: 4, first_name: 'Jean', last_name: "Jones", email: "jean_jones@gmail.com"}),
+      knex('users').insert({id: 5, first_name: 'Tudor', last_name: "Manole", email: "tmanole@gmail.com"}),
+      knex('users').insert({id: 6, first_name: 'Allen', last_name: "Harper", email: "allenharper@gmail.com"})
+
+    ])
+    .then(()=> {
       return Promise.all([
-        knex('users').insert({first_name: 'Vasiliy', last_name: "Klimkin", email: "vasily.klimkin@gmail.com"}),
-        knex('users').insert({first_name: 'John', last_name: "Lennie", email: "john.lennie@gmail.com"}),
-        knex('users').insert({first_name: 'David', last_name: "Chasteau", email: "david.chasteau@gmail.com"})
-        //knex('users').insert({id: 2, name: 'Bob'}),
-        //knex('users').insert({id: 3, name: 'Charlie'})
+        knex('events').insert({id: 1, event_name: "Vasily's big Party!", event_date: '2017-07-22', url: 'b2xVn2', admin_id: 1}),
+        knex('events').insert({id: 2, event_name: "Johns's RAVE!!!", event_date: '2017-07-02', url: 'L3xETx', admin_id: 2})
       ]);
-    });
+    })
+    .then(() => {
+      return Promise.all([
+        knex('participants').insert({id: 1, event_confirmation: 1, user_id: 3, event_id: 1}),
+        knex('participants').insert({id: 2, event_confirmation: 1, user_id: 4, event_id: 1}),
+        knex('participants').insert({id: 3, event_confirmation: 1, user_id: 5, event_id: 1})
+      ])
+    }).catch((err) => console.error(err));
 };
--- routes/events.js
+++ routes/events.js
@@ -14,6 +14,5 @@ module.exports = (knex) => {
         res.json(results);
     });
   });
-
   return router;
 }
