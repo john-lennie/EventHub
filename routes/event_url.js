0d5a295c5b963d697445ce3554d7e3bea62839d2
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-24 00:22:40 +0000

    Added the participants crude insert for now.

--- routes/event_url.js
+++ routes/event_url.js
@@ -24,13 +24,30 @@ module.exports = (dataHelpers) => {
     console.log("==>" + req.params.id);
     dataHelpers.publicGetSummary(req.params.id).then(function (query_response) {
       console.log(query_response);
-      res.render("test_user_page", query_response[0]);
+      let data = query_response[0];
+      res.render("test_user_page", {data});
 
 
     });
   });
 
-
+//STATIC PARTICIPANT POST
+  router.post("/:id/user", (req,res) => {
+    console.log("HELLO WORLD");
+    console.log("==>" + req.params.id);
+    dataHelpers.publicGetSummary(req.params.id).then(function (query_response) {
+      console.log(query_response);
+      let first_name = req.body.f_name;
+      let last_name = req.body.l_name;
+      let email = req.body.email;
+      let attending = true;
+      let event_id = query_response[0].id;
+      //console.log(first_name);
+      dataHelpers.insertParticipant(first_name, last_name, email, attending, event_id);
+    });
+    console.log(req.body);
+    res.send(200);
+  });
 
   router.post("/:id", (req, res) => {
     let selected_id = req.url.substring(1, req.url.length);
