089749c41806b54287ef76fc9cf8dc823eb29415
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-25 19:57:14 +0000

    Added error validation for create event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,14 @@ $(document).ready(() => {
   let form = $("#event_create_form");
   form.on("submit",( (event) => {
     event.preventDefault();
-    console.log(form.serialize());
+
+    if ($("#first_name")[0].value === '' ||  $("#last_name")[0].value === '' || $("#event_name")[0].value === '' ||
+          $("#date_calendar")[0].value === '' || $("#date_time")[0].value === '' || $("#description")[0].value ==='' || $("#email")[0].value === '')
+    {
+      $("#error").text("Everything must be filled");
+    }
+    else
+    {
       $.ajax({
         data: form.serialize(),
         url: form.attr('action'),
@@ -23,6 +30,8 @@ $(document).ready(() => {
           window.location = res;
         }
       });
-    }));
+    }
+
+  }));
 
 });
